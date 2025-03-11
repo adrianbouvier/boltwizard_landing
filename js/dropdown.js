a93279cd2382b40a0a2286d363aab2bc569954f1
Adrian <adrianbouvier@gmail.com>
2025-03-11 15:51:07 +0100

    actualizaxcion

---
+++ js/dropdown.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdown = document.getElementById('languageDropdown');
+    var dropdownMenu = document.getElementById('dropdownMenu');
+    var dropdownItems = document.querySelectorAll('.language-menu .dropdown-item');
+
+    function toggleDropdown() {
+        dropdownMenu.classList.toggle('show');
+    }
+
+    function closeDropdown() {
+        dropdownMenu.classList.remove('show');
+    }
+
+    dropdown.addEventListener('click', toggleDropdown);
+    dropdown.addEventListener('touchstart', toggleDropdown);
+
+    dropdownItems.forEach(function(item) {
+        item.addEventListener('click', closeDropdown);
+        item.addEventListener('touchstart', closeDropdown);
+    });
+});
